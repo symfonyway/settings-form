da1d32f1b5856630936f64ff971d1de3c39585bb
Nikita Ku <nikitabirulia@yandex.by>
2022-08-25 14:50:41 +0300

    Update layout with bootstrap-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,169 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import { Button } from 'react-bootstrap';
+
+
 const App = () => {
   return (
-    <div className='nsw'>
-      <div className='nsw__row'>
-        <div className='nsw__col'>
-          <h3>Ethernet Settings</h3>
-          
-          <input type="radio" className='nsw__input' id="automaticallyIp" name="ipVariant" value="1" checked/>
-          <label className='nsw__option-label' htmlFor="automaticallyIp">Obtain an IP address automatically (DHCP/BootP)</label>
+    <Container>
+      <Form>
+        <Row>
+          <Col className="border border-bottom-0 p-4">
+            <h3>Ethernet Settings</h3>
+            
+            <Form.Check 
+              type="radio"
+              id="automaticallyIP"
+              name="variantIP"
+              label="Obtain an IP address automatically (DHCP/BootP)"
+              checked
+            />
 
-          
-          <input type="radio" className='nsw__input' id="followingIp" name="ipVariant" value="0"/>
-          <label className='nsw__option-label' htmlFor="followingIp">Use the following IP:</label>
+            <Form.Check 
+              type="radio"
+              id="automaticallyIP"
+              name="variantIP"
+              label="Use the following IP:"
+            />
 
-          <div className='nsw__content'>
-            <label htmlFor="ipAddress" className='nsw__content-label'>IP address:</label>
-            <input type="text" className='nsw__content-input' id="ipAddress"/>
+            <Form.Group className='text-end' controlId="formBasicIP">
+              <Row>
+                <Col><Form.Label>IP address:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
 
-            <label htmlFor="subnetMask" className='nsw__content-label'>Subnet mask:</label>
-            <input type="text" className='nsw__content-input' id="subnetMask"/>
+              <Row>
+                <Col><Form.Label>Subnet mask:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
 
-            <label htmlFor="gateway" className='nsw__content-label'>Default Gateway:</label>
-            <input type="text" className='nsw__content-input' id="gateway"/>
-          </div>
+              <Row>
+                <Col><Form.Label>Default Gateway:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
+            </Form.Group>
 
-          
-          <input type="radio" className='nsw__input' id="autoDNS" name="DNSVariant" value="1" checked/>
-          <label className='nsw__option-label' htmlFor="autoDNS">Obtain DNS server address automatically</label>
+            <Form.Check 
+              type="radio"
+              id="autoDNS"
+              name="variantDNS"
+              label="Obtain DNS server address automatically"
+              checked
+            />
 
-          
-          <input type="radio" className='nsw__input' id="followingDNS" name="DNSVariant" value="0"/>
-          <label className='nsw__option-label' htmlFor="followingDNS">Use the following DNS server address:</label>
+            <Form.Check 
+              type="radio"
+              id="followingDNS"
+              name="variantDNS"
+              label="Use the following DNS server address:"
+            />
 
-          <div className='nsw__content'>
-            <label htmlFor="preferredDNS" className='nsw__content-label'>Preferred DNS server:</label>
-            <input type="text" className='nsw__content-input' id="preferredDNS"/>
+            <Form.Group className='text-end' controlId="formBasicDNS">
+              <Row>
+                <Col className="text-right"><Form.Label>Preferred DNS server:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
 
-            <label htmlFor="altDNS" className='nsw__content-label'>Alternative DNS server:</label>
-            <input type="text" className='nsw__content-input' id="altDNS"/>
-          </div>
-        </div>
-        <div className='nsw__col'>
-          <h3>Wireless Settings</h3>
-          
-          <input type="checkbox" id="wlessEnableWifi"/>
-          <label htmlFor="wlessEnableWifi">Enable Wifi:</label>
+              <Row>
+                <Col><Form.Label>Alternative DNS server:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
+            </Form.Group>
+          </Col>
+          <Col className="border border-bottom-0 border-right-0 p-4">
+            <h3>Wireless Settings</h3>
 
-          
-          <input type="checkbox" id="enableWifiSecurity"/>
-          <label htmlFor="enableWifiSecurity">Enable Wireless Security:</label>
+            <Form.Check 
+              type="checkbox"
+              id="wlessEnableWifi"
+              label="Enable wifi"
+            />
 
-          <input type="radio" className='nsw__input' id="wlessAutoIp" name="wlessIpVariant" value="1" checked/>
-          <label className='nsw__option-label' htmlFor="wlessAutoIp">Obtain an IP address automatically (DHCP/BootP)</label>
+            <Row>
+              <Col className='text-end'><Form.Label>Wireless Network Name:</Form.Label></Col>
+              <Col><Form.Control type="text"/></Col>
+            </Row>
 
-          
-          <input type="radio" className='nsw__input' id="wlessFollowingIp" name="wlessIpVariant" value="0"/>
-          <label className='nsw__option-label' htmlFor="wlessFollowingIp">Use the following IP:</label>
+            <Form.Check 
+              type="checkbox"
+              id="wlessEnableSecurity"
+              label="Enable Wireless Security"
+            />
 
-          <div className='nsw__content'>
-            <label htmlFor="wlessIpAddress" className='nsw__content-label'>IP address:</label>
-            <input type="text" className='nsw__content-input' id="wlessIpAddress"/>
+            <Row>
+              <Col className="text-end"><Form.Label>Security Key:</Form.Label></Col>
+              <Col><Form.Control type="text"/></Col>
+            </Row>
 
-            <label htmlFor="wlessSubnetMask" className='nsw__content-label'>Subnet mask:</label>
-            <input type="text" className='nsw__content-input' id="wlessSubnetMask"/>
+            <Form.Check 
+              type="radio"
+              id="automaticallyIP"
+              name="wlessIPVar"
+              label="Obtain an IP address automatically (DHCP/BootP)"
+              checked
+            />
 
-            <label htmlFor="wlessGateway" className='nsw__content-label'>Default Gateway:</label>
-            <input type="text" className='nsw__content-input' id="wlessGateway"/>
-          </div>
+            <Form.Check 
+              type="radio"
+              id="automaticallyIP"
+              name="wlessIPVar"
+              label="Use the following IP:"
+            />
 
-          
-          <input type="radio" className='nsw__input' id="wlessAutoDNS" name="wlessDNSVariant" value="1" checked/>
-          <label className='nsw__option-label' htmlFor="wlessAutoDNS">Obtain DNS server address automatically</label>
+            <Form.Group className='text-end' controlId="formWlessDNS">
+              <Row>
+                <Col><Form.Label>IP address:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
 
-          
-          <input type="radio" className='nsw__input' id="wlessFollowingDNS" name="wlessDNSVariant" value="0"/>
-          <label className='nsw__option-label' htmlFor="wlessFollowingDNS">Use the following DNS server address:</label>
+              <Row>
+                <Col><Form.Label>Subnet mask:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
 
-          <div className='nsw__content'>
-            <label htmlFor="wlessPreferredDNS" className='nsw__content-label'>Preferred DNS server:</label>
-            <input type="text" className='nsw__content-input' id="wlessPreferredDNS"/>
+              <Row>
+                <Col><Form.Label>Default Gateway:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
+            </Form.Group>
 
-            <label htmlFor="wlessAltDNS" className='nsw__content-label'>Alternative DNS server:</label>
-            <input type="text" className='nsw__content-input' id="wlessAltDNS"/>
-          </div>
-        </div>
-      </div>
-      <div className='row'></div>
-    </div>
+            <Form.Check 
+              type="radio"
+              id="autoDNS"
+              name="wlessDNSVar"
+              label="Obtain DNS server address automatically"
+              checked
+            />
+
+            <Form.Check 
+              type="radio"
+              id="followingDNS"
+              name="wlessDNSVar"
+              label="Use the following DNS server address:"
+            />
+
+            <Form.Group className='text-end' controlId="formWlessDNS">
+              <Row>
+                <Col className="text-right"><Form.Label>Preferred DNS server:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
+
+              <Row>
+                <Col><Form.Label>Alternative DNS server:</Form.Label></Col>
+                <Col><Form.Control type="text" /></Col>
+              </Row>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row xs="auto" className="border p-2">
+          <Col className='px-1'><Button variant="primary">Save</Button></Col>
+          <Col className='px-1'><Button variant="outline-primary">Cancel</Button></Col>
+        </Row>
+      </Form>
+    </Container>
   )
 }
 
